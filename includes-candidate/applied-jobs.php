34d5f63bfcb71a5424d2caeadef0178c43b09bd9
John Ray Legaspi <johnraylegaspi2@gmail.com>
2017-11-16 16:29:44 +0800

    FIX
    FIX

--- includes-candidate/applied-jobs.php
+++ includes-candidate/applied-jobs.php
@@ -4,49 +4,21 @@
 
         <div class="content-area recent-property" style="background-color: #FFF;">
             <div class="container">   
+            <div class="row">
+                    <div class="col-md-10 col-md-offset-1 col-sm-12 text-center ">
+                        <!-- /.feature title -->
+                        <h2>MY APPLIED JOBS</h2>
+                       
+                    </div>
+                </div>
                 <div class="row">
-                     <div class="col-md-1 pr-30 padding-top-40 properties-page user-properties">
-                     </div>
-                    <div class="col-md-10 pr-30 padding-top-40 properties-page user-properties">
+                   
+                    <div class="col-md-12 pr-30 padding-top-40 properties-page user-properties">
+
+                        
 
                         <div class="section"> 
-                            <div class="page-subheader sorting pl0 pr-10">
-
-
-                                <ul class="sort-by-list pull-left">
-                                    <li class="active">
-                                        <a href="javascript:void(0);" class="order_by_date" data-orderby="property_date" data-order="ASC">
-                                            Posted Date <i class="fa fa-sort-amount-asc"></i>					
-                                        </a>
-                                    </li>
-                                    <li class="">
-                                        <a href="javascript:void(0);" class="order_by_price" data-orderby="property_price" data-order="DESC">
-                                            Posted Date <i class="fa fa-sort-numeric-desc"></i>						
-                                        </a>
-                                    </li>
-                                </ul><!--/ .sort-by-list-->
-
-                                <div class="items-per-page pull-right">
-                                    <label for="items_per_page"><b>Property per page :</b></label>
-                                    <div class="sel">
-                                        <select id="items_per_page" name="per_page">
-                                            <option value="3">3</option>
-                                            <option value="6">6</option>
-                                            <option value="9">9</option>
-                                            <option selected="selected" value="12">12</option>
-                                            <option value="15">15</option>
-                                            <option value="30">30</option>
-                                            <option value="45">45</option>
-                                            <option value="60">60</option>
-                                        </select>
-                                    </div><!--/ .sel-->
-                                </div><!--/ .items-per-page-->
-                            </div>
-
-                        </div>
-
-                        <div class="section"> 
-                            <div id="list-type" class="proerty-th-list">
+                            <div id="list-type" class="proerty-th">
                                                       
 
                             <?php  
@@ -132,8 +104,7 @@
                         </div>
 
                     </div>       
-                            <div class="col-md-1 pr-30 padding-top-40 properties-page user-properties">
-                     </div>
+                         
                     
                 </div>
             </div>
