471dfee77b67d7cd5262ecd3ea1e1a6babf7df7d
ajparadela <ajparadela@gmail.com>
2017-11-23 10:51:23 +0800

    footer fix
    footer fix

--- includes-agency/footer.php
+++ includes-agency/footer.php
@@ -12,7 +12,7 @@
 
                                 <p><strong>Lazzy Works</strong> is an innovative job and employee finder created by Lazzy Technologies that let customer choose their desired preferences like skills, age, health status, and location for a wide range of Employements in your selected country.</p>
                                 <ul class="footer-adress">
-                                    <li><i class="pe-7s-map-marker strong">Room 510, Global Gateway, Tower, 63 Wing Hong Street, Lai Chi Kok, KLN, Hong Kong</i></li>
+                                    <!-- <li><i class="pe-7s-map-marker strong">Room 510, Global Gateway, Tower, 63 Wing Hong Street, Lai Chi Kok, KLN, Hong Kong</i></li> -->
                                     <li><i class="pe-7s-mail strong"> support@lazzyworks.com</i></li>
                                     <li><i class="pe-7s-call strong"> +1 999 999 9999</i> </li>
                                 </ul>
@@ -35,9 +35,9 @@
                         
                                 <div class="social pull-right"> 
                                     <ul><br><br><br><br><br><br><br><br><br><br>
-                                        <li><a class="wow fadeInUp animated" href="https://twitter.com/lazzytechnology"><i class="fa fa-twitter"></i></a></li>
+                                        <!-- <li><a class="wow fadeInUp animated" href="https://twitter.com/lazzytechnology"><i class="fa fa-twitter"></i></a></li> -->
                                         <li><a class="wow fadeInUp animated" href="https://www.facebook.com/Lazzy-Technologies-LTD-1763745776977014/?ref=br_rs" data-wow-delay="0.2s"><i class="fa fa-facebook"></i></a></li>
-                                        <li><a class="wow fadeInUp animated" href="https://plus.google.com/u/0/105292546621754819289" data-wow-delay="0.3s"><i class="fa fa-google-plus"></i></a></li>
+                                        <!-- <li><a class="wow fadeInUp animated" href="https://plus.google.com/u/0/105292546621754819289" data-wow-delay="0.3s"><i class="fa fa-google-plus"></i></a></li> -->
                                         <li><a class="wow fadeInUp animated" href="https://www.instagram.com/lazzytechnologies/" data-wow-delay="0.4s"><i class="fa fa-instagram"></i></a></li>
                                         <li><a class="wow fadeInUp animated" href="www.linkedin.com/in/lazzy-technologies" data-wow-delay="0.6s"><i class="fa fa-linkedin"></i></a></li>
                                     </ul> 
